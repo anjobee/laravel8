05a5b4302cdb3b78ea492116f7085c683bf6035e
Angelo Bihis <anjo.bihis@gmail.com>
2021-02-10 02:10:21 +0800

    on lesson 17

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,11 +25,13 @@ use Illuminate\Support\Facades\Route;
 //     return view('home.contact');
 // })->name('home.contact');
 
-Route::view('/', 'home.index')
+Route::get('/', [HomeController::class, 'home'])
     ->name('home.index');
 
-Route::view('/contact', 'home.contact')
+Route::get('/contact', [HomeController::class, 'contact'])
     ->name('home.contact');
+
+Route::get('/single', AboutController::class);
 
 $posts = [
     1 => [
@@ -47,50 +52,53 @@ $posts = [
     ]
 ];
 
-Route::get('/posts', function() use($posts) {
-    // dd(request()->all());
-    dd((int)request()->query('limit', 1));
-    return view('posts.index', ['posts' => $posts]);
-});
+Route::resource('posts', PostsController::class)
+    ->only(['index', 'show', 'create', 'store']);
 
-Route::get('/posts/{id}', function($id) use($posts) {
-    abort_if(!isset($posts[$id]), 404);
+// Route::get('/posts', function() use($posts) {
+//     // dd(request()->all());
+//     dd((int)request()->query('limit', 1));
+//     return view('posts.index', ['posts' => $posts]);
+// });
 
-    return view('posts.show', ['post' => $posts[$id]]);
-})
-// ->where([
-//     'id' => '[0-9]+'
-// ])
-->name('posts.show');
+// Route::get('/posts/{id}', function($id) use($posts) {
+//     abort_if(!isset($posts[$id]), 404);
 
-Route::get('/recent-posts/{days_ago?}', function($daysAgo = 20) {
-    return "Posts from $daysAgo days ago.";
-})->name('posts.recent.index')->middleware('auth');
+//     return view('posts.show', ['post' => $posts[$id]]);
+// })
+// // ->where([
+// //     'id' => '[0-9]+'
+// // ])
+// ->name('posts.show');
 
-Route::prefix('/fun')->name('fun.')->group(function() use($posts) {
-    Route::get('/responses', function() use($posts) {
-        return response($posts, 201)
-            ->header('Content-Type', 'application/json')
-            ->cookie('MY_COOKIE', 'Anjo Bihis', 3600);
-    })->name('responses');
+// Route::get('/recent-posts/{days_ago?}', function($daysAgo = 20) {
+//     return "Posts from $daysAgo days ago.";
+// })->name('posts.recent.index')->middleware('auth');
 
-    Route::get('/redirect', function() {
-        return redirect('/contact');
-    })->name('redirect');
+// Route::prefix('/fun')->name('fun.')->group(function() use($posts) {
+//     Route::get('/responses', function() use($posts) {
+//         return response($posts, 201)
+//             ->header('Content-Type', 'application/json')
+//             ->cookie('MY_COOKIE', 'Anjo Bihis', 3600);
+//     })->name('responses');
 
-    Route::get('/named-route', function() {
-        return redirect()->route('posts.show', ['id' => 1]);
-    })->name('named-route');
+//     Route::get('/redirect', function() {
+//         return redirect('/contact');
+//     })->name('redirect');
 
-    Route::get('/away', function() {
-        return redirect()->away('https://google.com');
-    })->name('away');
+//     Route::get('/named-route', function() {
+//         return redirect()->route('posts.show', ['id' => 1]);
+//     })->name('named-route');
 
-    Route::get('/json', function() use($posts) {
-        return response()->json($posts);
-    })->name('json');
+//     Route::get('/away', function() {
+//         return redirect()->away('https://google.com');
+//     })->name('away');
 
-    Route::get('/download', function() use($posts) {
-        return response()->download(public_path('/luffy.jpg'), 'face.jpg');
-    })->name('download');
-});
+//     Route::get('/json', function() use($posts) {
+//         return response()->json($posts);
+//     })->name('json');
+
+//     Route::get('/download', function() use($posts) {
+//         return response()->download(public_path('/luffy.jpg'), 'face.jpg');
+//     })->name('download');
+// });
